87ffcf4e5883fb53e003166395d7b5ede713659c
Hanzo <hanzo_s_w@hotmail.com>
2021-01-25 23:36:17 +0200

    Russian translation added

---
+++ inc/languages/russian/prostats.lang.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ProStats Plugin - English language
+ */
+
+$l['prostats_prostats'] = 'Статистика форума';
+$l['prostats_reload'] = 'Перегрузить';
+
+$l['prostats_username'] = 'Имя пользователя';
+$l['prostats_posts'] = 'Сообщения';
+$l['prostats_datetime'] = 'Число, дата';
+$l['prostats_forum'] = 'Форум';
+
+$l['prostats_latest_posts'] = 'Последние сообщения';
+$l['prostats_most_replies'] = 'Больше всего ответов';
+$l['prostats_most_reputations'] = 'Наивысшая репутация';
+$l['prostats_most_thanks'] = 'Топ по благодарностям';
+$l['prostats_most_views'] = 'Самое просматриваемые';
+$l['prostats_newest_members'] = 'Новые пользователи';
+$l['prostats_top_downloads'] = 'Самое скачиваемое';
+$l['prostats_top_posters'] = 'Топ писателей';
+$l['prostats_top_thread_posters'] = 'Топ писателей по темам';
+$l['prostats_top_topreferrers'] = 'Топ по рефералам';
+
+$l['prostats_topic'] = 'Тема';
+$l['prostats_author'] = 'Автор';
+$l['prostats_last_sender'] = 'Последний пославший';
+$l['prostats_replies'] = 'Ответы';
+
+$l['prostats_views'] = 'Просмотры';
+$l['prostats_subject'] = 'Название';
+
+$l['prostats_err_thxplugin'] = 'Пожалуйста установите <a href="http://prostats.wordpress.com/support/">Thanks</a> plugin.';
+$l['prostats_err_undefind'] = 'Не определено';
+
+$l['prostats_disabled'] = 'Вы не можете использовать функционал prostats так как это отключено Администратором..';
